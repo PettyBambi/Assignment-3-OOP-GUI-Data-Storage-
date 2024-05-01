cb71925d15e94bab26fc567f89dc55b87c490b85
PettyBambi <dabeyah.17@gmail.com>
2024-05-01 18:30:32 +0400

    Created Client class

---
+++ Client.py
@@ -0,0 +1,37 @@
+from Person import Person
+
+
+class Client(Person):
+    """Class to represent a client"""
+
+    def __init__(self, client_id, person_id, first_name, last_name, email, phone, birth_of_date, age, gender,
+                 nationality, budget):
+        Person.__init__(self, person_id, first_name, last_name, email, phone, birth_of_date, age, gender, nationality)
+        self._client_id = client_id
+        self._budget = budget
+
+    # setters and getters
+    def set_client_id(self, client_id):
+        self._client_id = client_id
+    def get_client_id(self):
+        return self._client_id
+
+    def set_budget(self, budget):
+        self._budget = budget
+    def get_budget(self):
+        return self._budget
+
+    def get_client_info(self):
+        client_info = {
+            'Client ID': self._client_id,
+            'Name': self.get_name(),
+            'Email': self.get_email(),
+            'Phone': self.get_phone(),
+            'Budget': self._budget
+        }
+        return client_info
+
+    def __str__(self):
+        return "Client ID: " + str(
+            self._client_id) + ", Name: " + self.get_name() + ", Email: " + self.get_email() + ", Phone: " + str(
+            self.get_phone()) + ", Budget: " + str(self._budget)
